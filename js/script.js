4c181884c4c19e2c0477b24f45505684a6ae37d7
JoseCMessias <josecmessias22@gmail.com>
2023-12-22 14:51:06 -0300

    Página inicial do quizz

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
+const form = document.querySelector('.login-form');
+const button = document.querySelector('.login__button');
+
+const handleSubmit = (event) => {
+   event.preventDefault();
+   
+   window.location = 'question1.html';
+}
+
 function mudarPagina(novaPagina) {
-    window.location.href = novaPagina;
-  }
+  window.location.href = novaPagina;
+}
+
+form.addEventListener('submit', handleSubmit);
   
