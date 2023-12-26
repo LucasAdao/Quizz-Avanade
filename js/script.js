95e7c240ed4bde760e19365b3fd59ae7b26140a8
LucasAdao <l.adao@outlook.com>
2023-12-26 16:51:36 -0300

    Update Modulos - Direcionamento,Dicas

--- js/script.js
+++ js/script.js
@@ -1,51 +1,11 @@
-
-const form = document.querySelector('.login-form');
-const button = document.querySelector('.login__button');
-
-const handleSubmit = (event) => {
-   event.preventDefault();
-   
-   window.location = 'question1.html';
+function questaoCorreta(novaPagina) {
+    window.location.href = novaPagina;
+    alert("Alternativa Correta!! :D")
 }
-
-function mudarPagina(novaPagina) {
-  window.location.href = novaPagina;
+function questaoIncorreta(novaPagina) {
+    window.location.href = novaPagina;
+    alert("Alternativa Incorreta! D:")
 }
-
-form.addEventListener('submit', handleSubmit);
-  
-
-let pontuacao = 0;
-
-function mudarPagina(novaPagina, respostaCorreta) {
-        if (respostaCorreta) {
-            pontuacao++;
-            localStorage.setItem('pontuacao', pontuacao);
-            window.location.href = novaPagina;
-        } else {
-            alert('Ops, a resposta não está correta...');
-            window.location.href = novaPagina;
-        }
+function startButton(novaPagina) { 
+    window.location.href = novaPagina;
 }
-
-function mudarPaginaDesc(novaPagina) {
-  window.location.href = novaPagina;
-}
-
-function reiniciarQuiz(novaPagina) {
-  window.location.href = novaPagina;
-  localStorage.clear();
-}
-
-window.onload = function() {
-    const pontuacaoFinalElement = document.getElementById('pontuacaoFinal');
-    const pontuacaoSalva = localStorage.getItem('pontuacao');
-
-    if (pontuacaoSalva !== null) {
-        pontuacao = parseInt(pontuacaoSalva);
-        pontuacaoFinalElement.textContent = pontuacao.toString();
-    } else {
-        pontuacaoFinalElement.textContent = 'Zero';
-    }
-};
-
